b67515c8f2eed5e86d0ac3e332bc758751a917eb
Anuj Shama <anujsharma787764@gmail.com>
2025-09-06 21:31:16 +0530

    number

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import GlobalProvider from "@/components/Application/GlobalProvider";
 import "./globals.css";
 import { Assistant } from 'next/font/google'
 import { ToastContainer } from 'react-toastify';
+import Analytics from "@/components/Application/Analytics"
 const assistantFont = Assistant({
   weight: ['400', '500', '600', '700', '800'],
   subsets: ['latin'],
@@ -9,8 +10,8 @@ const assistantFont = Assistant({
 })
 
 export const metadata = {
-  title: "STYLITHIC FASHIONS | Premium Online Shopping Experience",
-  description: "Discover STYLITHIC FASHIONS – your one-stop destination for premium clothing, accessories, and lifestyle products. Shop the latest trends with fast delivery, secure payments, and unmatched quality.",
+  title: "Stylithic Fashions | Premium Online Shopping Experience",
+  description: "Discover Stylithic Fashions – your one-stop destination for premium clothing, accessories, and lifestyle products. Shop the latest trends with fast delivery, secure payments, and unmatched quality.",
   icons: {
     icon: "/favicon.ico", // public/ folder me apna logo
   },
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       >
         <GlobalProvider>
           <ToastContainer />
+          <Analytics /> 
           {children}
         </GlobalProvider>
       </body>
