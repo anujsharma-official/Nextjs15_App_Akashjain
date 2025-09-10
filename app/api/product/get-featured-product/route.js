384e0e15623b3cc0fd10a06fa01a98cc1ccc0597
Anuj Shama <anujsharma787764@gmail.com>
2025-09-10 15:54:59 +0530

    number

--- app/api/product/get-featured-product/route.js
+++ app/api/product/get-featured-product/route.js
@@ -3,18 +3,31 @@ import { catchError, response } from "@/lib/helperFunction";
 import ProductModel from "@/models/Product.model";
 import MediaModel from "@/models/Media.model";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+
 export async function GET() {
     try {
         await connectDB()
 
-        const getProduct = await ProductModel.find({ deletedAt: null }).populate('media').limit(8).lean()
+        const getProduct = await ProductModel.find().populate('media').limit(8).lean()
 
-        if (!getProduct) {
+        if (!getProduct || getProduct.length === 0) {
             console.log('product not found');
             return response(false, 404, 'Product not found.')
         }
 
-        return response(true, 200, 'Product found.', getProduct)
+        return new Response(
+            JSON.stringify({ success: true, data: getProduct }),
+            {
+                status: 200,
+                headers: {
+                    "Cache-Control":
+                        "no-store, no-cache, must-revalidate, proxy-revalidate",
+                },
+            }
+        );
 
     } catch (error) {
         return catchError(error)
